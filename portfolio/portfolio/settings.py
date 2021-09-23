3a47d8597d47304f692c79b67df4cec6d4e32820
apoorv-x12 <fifa.ajay219@gmail.comclear>
2021-09-24 00:02:19 +0530

    completed setup

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'portfolio.urls'
@@ -122,6 +123,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS= (
+ os.path.join(BASE_DIR,'static'),
+) # comma is important at last since this is a tuple
+
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
