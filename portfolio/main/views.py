a6066bca1fb75840bc3adaa13fa4673a946d1269
apoorv-x12 <fifa.ajay219@gmail.comclear>
2021-09-28 00:59:18 +0530

    completed project added json file and DATA to dynamically generate data along with django template library

--- portfolio/main/views.py
+++ portfolio/main/views.py
@@ -1,19 +1,25 @@
 from django.shortcuts import render
+from django.conf import settings
 
 # Create your views here.
 def index(request):
-    context={}
+    name=settings.DATA['NAME']
+    about_me=settings.DATA1['ABOUT_ME']
+    context={'name':name, "about_me":about_me}
     return render(request,'main/index.html',context)
 
 def projects(request):
-    context={}
+    projects=settings.DATA['PROJECTS']
+    context={'projects':projects}
     return render(request,'main/projects.html',context)
 
 def languages(request):
-    context={}
+    languages=settings.DATA1['LANGUAGES']
+    context={'languages':languages}
     return render(request,'main/languages.html',context)
 
 
 def books(request):
-    context={}
+    books_summary=settings.DATA['BOOKS_SUMMARY']
+    context={"books_summary":books_summary}
     return render(request,'main/books.html',context)
