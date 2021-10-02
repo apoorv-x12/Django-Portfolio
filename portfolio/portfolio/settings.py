6f51c2e2764efedef2d7859b2895a97c8dee3140
apoorv-x12 <fifa.ajay219@gmail.comclear>
2021-10-02 19:51:22 +0530

    changed settings.py a little for deployment

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7m5t=#4e#q@uj=!l0t8^kdp291hy1lu#7)6p@8@ch-k^hn54k^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','apoorv123xyz.pythonanywhere.com']
 
 
 # Application definition
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'), 
     }
 }
 
